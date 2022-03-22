503a0577b634cbcea1628f8b1687cd8c83044f37
KncskBenedek <koncsikbenedek@gmail.com>
2022-03-22 20:05:35 +0100

    row() működik

--- script.js
+++ script.js
@@ -5,70 +5,64 @@ window.addEventListener("load", init);
 O
 X
 */
-
+var nyertesElem = "";
 var tomb = [];
-var hossz = 9 // majd változ
+var hossz = 9; // majd változ
 var hanyadikLepes = 0;
-function ID(elem){
+function ID(elem) {
     return document.getElementById(elem);
 }
-function CLASS(elem){
+function CLASS(elem) {
     return document.getElementsByClassName(elem);
 }
 
-function $(elem){
+function $(elem) {
     return document.querySelectorAll(elem);
 }
 
-function init(){
+function init() {
     tombFeltolt(hossz);
     alapAllas();
     eventek();
 }
 
-
-
-
-function tombFeltolt(db){
+function tombFeltolt(db) {
     for (let index = 0; index < db; index++) {
-        tomb.push("-")
+        tomb.push("-");
     }
-
 }
 
-function kiir(hova, txt){
+function kiir(hova, txt) {
     ID(hova).innerHTML = txt;
 }
 
-function alapAllas(){
-    
+function alapAllas() {
     var txt = "";
     for (let index = 0; index < hossz; index++) {
-        txt += `<div id="${index}">${tomb[index]}</div>`
-        
+        txt += `<div id="${index}">${tomb[index]}</div>`;
     }
-    kiir("nagyDiv", txt)
+    kiir("nagyDiv", txt);
 }
-function eventek(){
+function eventek() {
     for (let index = 0; index < hossz; index++) {
         ID(index).addEventListener("click", berak);
     }
 }
-function berak(){
+function berak() {
     var hol = Number(event.target.id);
-    var alakzat = kiJon()?"O":"X";
-     ID(hol).innerText = alakzat;
-     tomb[hol] = alakzat;
-     ID(hol).removeEventListener("click", berak);
-    hanyadikLepes ++;
-    if (hanyadikLepes<4) {
+    var alakzat = kiJon() ? "O" : "X";
+    ID(hol).innerText = alakzat;
+    tomb[hol] = alakzat;
+    ID(hol).removeEventListener("click", berak);
+    hanyadikLepes++;
+    if (hanyadikLepes > 4) {
         ellenorzes();
     }
 }
 
-function kiJon(){
+function kiJon() {
     var parosE;
-    if (hanyadikLepes % 2 === 0){
+    if (hanyadikLepes % 2 === 0) {
         parosE = true;
     } else {
         parosE = false;
@@ -76,29 +70,33 @@ function kiJon(){
     return parosE;
 }
 
-function ellenorzes(){
+function ellenorzes() {
     row();
-    column();
-    cross();
+    //column();
+    //cross();
 }
 
-function row(){
+function row() {
     var n = 0;
     var i = 0;
-    var sorHossz = 3
-    
+    var sorHossz = 3;
+
     var vanENyertes = false;
-    while(n < sorHossz &&!(vanENyertes)){
+    while (n < sorHossz && !vanENyertes) {
         var mettol = n * sorHossz;
-        var meddig = (mettol + sorHossz) - 1;
+        var meddig = mettol + sorHossz;
         i = mettol;
 
-        while(i < meddig && (tomb[mettol] === tomb[i]) && !(tomb[i] == "-") ){
-        i++;
+        while (i < meddig && (tomb[mettol] === tomb[i]) && !(tomb[i] == "-")) {
+            i++;
         }
-        vanENyertes = !(i < meddig)
+        vanENyertes = !(i < meddig);
         n++;
     }
+    if(vanENyertes){
+        nyertesElem = tomb[mettol]
+        console.log(nyertesElem);
+    }
     console.log(vanENyertes);
-    return vanENyertes   
+    return vanENyertes;
 }
